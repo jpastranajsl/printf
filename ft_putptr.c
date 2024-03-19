8d99d2e996b1430094d81525fac8c0b682c49d74
Jimena Pastrana <jpastran@c3r6s4.42prague.com>
2024-03-19 15:36:24 +0100

    fixed

--- ft_putptr.c
+++ ft_putptr.c
@@ -6,18 +6,24 @@
 /*   By: jpastran <jpastran@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 15:32:02 by jimenapastr       #+#    #+#             */
-/*   Updated: 2024/03/19 14:38:47 by jpastran         ###   ########.fr       */
+/*   Updated: 2024/03/19 15:07:44 by jpastran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+# include <stdarg.h>
+# include <stdint.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
 
 int	ft_getptr(uintptr_t u)
 {
 	char	*hex;
 	int		result;
 
-	hex = "123456789abcdef";
+	hex = "0123456789abcdef";
 	result = 0;
 	if (u >= 16)
 	{
@@ -31,7 +37,7 @@ int	ft_getptr(uintptr_t u)
 	return (result);
 }
 
-int	ft_putptr(uintptr_t ptr)
+int	ft_putptr(unsigned long long ptr)
 {
 	int	result;
 
